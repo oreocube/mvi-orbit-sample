26e17b85e197ab01680c0a4bbfa2b4fdfdba0338
oreocube <oreocube13@gmail.com>
2023-03-30 16:44:01 +0900

    refactor: mvi 구현

--- app/src/main/java/com/example/orbitsampleapp/CounterViewModel.kt
+++ app/src/main/java/com/example/orbitsampleapp/CounterViewModel.kt
@@ -3,15 +3,19 @@ package com.example.orbitsampleapp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class CounterViewModel : ViewModel() {
-    private val _state = MutableStateFlow(CounterState())
-    val state = _state.asStateFlow()
+
+    private val actionChannel = Channel<CounterAction>()
+    val state = actionChannel.receiveAsFlow()
+        .map(::reduce)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = CounterState()
+        )
 
     private val _sideEffect = Channel<CounterSideEffect>()
     val sideEffect = _sideEffect.receiveAsFlow()
@@ -19,9 +23,7 @@ class CounterViewModel : ViewModel() {
     fun increase() {
         viewModelScope.launch {
             if (state.value.count < 10) {
-                reduce { prev ->
-                    prev.copy(count = prev.count + 1)
-                }
+                dispatch(CounterAction.Increment)
             } else {
                 postSideEffect(CounterSideEffect.Toast("10 이하여야 합니다."))
             }
@@ -31,20 +33,34 @@ class CounterViewModel : ViewModel() {
     fun decrease() {
         viewModelScope.launch {
             if (state.value.count > 0) {
-                reduce { prev ->
-                    prev.copy(count = prev.count - 1)
-                }
+                dispatch(CounterAction.Decrement)
             } else {
                 postSideEffect(CounterSideEffect.Toast("0 이상이어야 합니다."))
             }
         }
     }
 
-    private fun reduce(reducer: (CounterState) -> CounterState) {
-        _state.update(reducer)
+    private suspend fun dispatch(event: CounterAction) {
+        actionChannel.send(event)
+    }
+
+    private fun reduce(action: CounterAction): CounterState {
+        return when (action) {
+            is CounterAction.Increment -> {
+                state.value.copy(count = state.value.count + 1)
+            }
+            is CounterAction.Decrement -> {
+                state.value.copy(count = state.value.count - 1)
+            }
+        }
     }
 
     private suspend fun postSideEffect(sideEffect: CounterSideEffect) {
         _sideEffect.send(sideEffect)
     }
+}
+
+sealed class CounterAction {
+    object Increment : CounterAction()
+    object Decrement : CounterAction()
 }
